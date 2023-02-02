1da784c6fe6e6809ccb66d692cacd0d243257e64
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-02-01 22:54:06 -0800

    added music, videos, music and sign up links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
@@ -16,10 +17,33 @@ function Navbar() {
 <div className='menu-icon' onClick={handleClick}>
     <i className={click ? 'fas  fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
+                Artist
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
+                Videos
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
+                Music
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+
+          </ul>
         </div>
       </nav>
     </>
   ); 
-}
+} 
  
 export default Navbar;
