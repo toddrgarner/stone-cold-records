7ee0419394da2bcc78c6deedd9042053c81a342f
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-02-01 16:39:41 -0800

    updated navbar.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Routes, Route } from 
+'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router>
+     <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+     </Router>
+  </>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <nav className="narbar">
+        <div className="navbar-container">
+<Link to="/" className="navbar-logo">
+    Stone Cold Records
+</Link>
+        </div>
+      </nav>
+    </>
+  ) 
+}
+
+export default Navbar
