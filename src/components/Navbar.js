c78bd7c8c5f271b941cc78ab5ed31e04096ea24b
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-02-02 15:56:41 -0800

    added btn styling to button.ccc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,65 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-      <nav className='narbar'>
-        <div className='navbar-container'>
-<Link to="/" className='navbar-logo'>
-    Stone Cold Records 
-</Link>
-<div className='menu-icon' onClick={handleClick}>
-    <i className={click ? 'fas  fa-times' : 'fas fa-bars'} />
+      <nav className="narbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Stone Cold Records
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas  fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='artist' className='nav-links' onClick={closeMobileMenu}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="artist" className="nav-links" onClick={closeMobileMenu}>
                 Artist
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='videos' className='nav-links' onClick={closeMobileMenu}>
+            <li className="nav-item">
+              <Link to="videos" className="nav-links" onClick={closeMobileMenu}>
                 Videos
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='music' className='nav-links' onClick={closeMobileMenu}>
+            <li className="nav-item">
+              <Link to="music" className="nav-links" onClick={closeMobileMenu}>
                 Music
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='sign up' className='nav-links-mobile' onClick={closeMobileMenu}>
+            <li className="nav-item">
+              <Link
+                to="sign up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
                 Sign Up
               </Link>
-            </li> 
-
+            </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</ 
+                    Button>}
         </div>
       </nav>
     </>
-  ); 
-} 
- 
-export default Navbar;
+  );
+}
+
+export default Navbar; 
