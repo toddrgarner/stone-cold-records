db5b76f58496c5050bf6b2874d857d97a696f749
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-02-22 17:31:05 -0800

    added Footer.js Footer.css and modified HeroSection.js

---
+++ src/elements/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div className='footer-container'>
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+            Join Stone Cold Records newsletter to receive info on
+            our artist and music
+        </p>
+        <p className="footer-subscription-text">
+            You can unsuscribe at anytime.
+        </p>
+        <div className="input-areas">
+        <form>
+            <input type="email"  name="email" placeholder="Your Email"
+            className="footer-input" />
+        </form>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default footer
--- src/elements/HeroSection.js
+++ src/elements/HeroSection.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import {Button} from './Button';
-import './HeroSection.css';
-import '../App.css';
-
-
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
 
 function HeroSection() {
   return (
-   <div className='hero-container'>
-      <video src='https://www.youtube.com/watch?v=SfhDhoktQ2M' autoPlay loop muted />
-    <h1>Stone Cold Records</h1>
-    <p>Featured Video</p>
-    <p>"Take You To A Place"</p>
-    <div className="hero-btns">
-      <Button clasName='btns' 
-      buttonStyle='btn--outline'
-      buttonSize='btn--large'
-      >
-        GET STARTED
-         </Button> 
-         <Button 
-         className='btns' 
-         buttonStyle='btn--primary'
-         buttonSize='btn--large'
-         >
-          WATCH VIDEO <i className='far fa-play-circle'/>
-         </Button>         
-       </div>
+    <div className="hero-container">
+      <video
+        src="https://www.youtube.com/watch?v=SfhDhoktQ2M"
+        autoPlay
+        loop
+        muted
+      />
+      <h1>Stone Cold Records</h1>
+      <p>Featured Video</p>
+      <p>"Take You To A Place"</p>
+      <div className="hero-btns">
+        <Button
+          clasName="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH VIDEO <i className="far fa-play-circle" />
+        </Button>
+      </div>
     </div>
   );
 }
