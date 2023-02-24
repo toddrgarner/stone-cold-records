52570dc8e78e58a6496c0a6ebc208318e493a7d1
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-02-23 17:19:41 -0800

    modified Footer.js

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -1,40 +1,53 @@
-import React from 'react'
-import './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 function footer() {
   return (
-    <div className='footer-container'>
+    <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-heading">
-            Join Stone Cold Records newsletter to receive info on
-            our artist and music
+          Join Stone Cold Records newsletter to receive info on our artist and
+          music
         </p>
         <p className="footer-subscription-text">
-            You can unsuscribe at anytime.
+          You can unsuscribe at anytime.
         </p>
         <div className="input-areas">
-        <form>
-            <input type="email"  name="email" placeholder="Your Email"
-            className="footer-input" />
-             <Button buttonStyle="btn--outline">Subscribe</Button>
-        </form>
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
         </div>
       </section>
       <div className="footer-links">
-      <div className="footer-link-wrapper">
-        <div className="footer-link-items">
-        <h2>About Us</h2>
-        <Link to='/sign-up'>How it works</Link>
-  
-
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Terms of Service</Link>
+            <Link to="/">Investors</Link>
+          </div>
+         <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">YouTube</Link>
+            <Link to="/">Twitter</Link>
+          </div>
         </div>
       </div>
-
-      </div>
     </div>
-  )
+  );
 }
 
 export default footer;
