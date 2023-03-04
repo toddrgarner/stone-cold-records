b0d01fc49e23f69a196c0752e92b666226daa613
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-03-04 12:46:49 -0800

    modified Navbar.js Navbar.css

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Stone Cold Records
-        <img className src={ Logo } alt="label-logo" />
+        <img src={ Logo } className='label-logo' alt="logo" />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
