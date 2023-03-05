a311d5d31abe195056f364cea8b69768ee2c19d4
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-03-04 16:27:31 -0800

    modifierd Navbar.js Navbar.css Footer.css

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from "react-router-dom";
 import './Navbar.css';
-import Logo from '../img/logo-11.png';
+import Logo from '../img/logo-16.png';
 
 
 
